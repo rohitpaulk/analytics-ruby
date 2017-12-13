d714f6351bd96069a05b99478169cb29fe6092ec
Paul Kuruvilla <rohitpaulk@gmail.com>
2017-12-13 17:14:34 +0530

    Use separate set of keys to test

--- spec/segment/analytics/e2e_spec.rb
+++ spec/segment/analytics/e2e_spec.rb
@@ -5,11 +5,13 @@ module Segment
   # webhook connected to the source (configured manually via the app) is able
   # to receive the data sent by this library.
   describe 'End-to-end tests', e2e: true do
-    WRITE_KEY = 'qhdMksLsQTi9MES3CHyzsWRRt4ub5VM6'
-    RUNSCOPE_BUCKET_KEY = 'pwb8mcmfks0f'
+    WRITE_KEY = 'kzt5bZFLzc0sl8qBoJ7j7zcv512Z5MpM'
+    RUNSCOPE_BUCKET_KEY = 'gefzlpt8ao5r'
 
     let(:client) { Segment::Analytics.new(write_key: WRITE_KEY) }
-    let(:runscope_client) { RunscopeClient.new(ENV.fetch('RUNSCOPE_TOKEN')) }
+    let(:runscope_client) {
+      RunscopeClient.new('46adda7b-a85e-4842-b6af-1785e0a8049a')
+    }
 
     it 'tracks events' do
       id = SecureRandom.uuid
